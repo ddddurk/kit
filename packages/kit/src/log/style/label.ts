3514af0f196ff0840503cb56ce471c1a2b1ca4c8
ddddurk <ddddurk@proton.me>
2023-04-30 22:50:03 -0400

    feat(@ddddurk/kit): add white option for label

--- packages/kit/src/log/style/label.ts
+++ packages/kit/src/log/style/label.ts
@@ -1,14 +1,17 @@
 import chalk from "chalk";
 
-import { MARGIN, SPACE } from "../../constants";
+import { MARGIN, SPACE } from "../constants";
 
-export const label = (hex: string, text: string) => {
+export const label = (
+  hex: string,
+  text: string,
+  white = true
+) => {
   const maxWidth = MARGIN - SPACE * 2;
 
   text = text.slice(0, maxWidth);
 
-  return (
-    " ".repeat(maxWidth - text.length) +
-    chalk.bgHex(hex).white.bold(" " + text + " ")
-  );
+  return " ".repeat(maxWidth - text.length) + white
+    ? chalk.bgHex(hex).white.bold(" " + text + " ")
+    : chalk.bgHex(hex).black.bold(" " + text + " ");
 };
